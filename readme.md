0b829ada80534626142825da919033c18a21b7b8
Carlos Rogerio Orioli <carlos.orioli@groweag.com>
2025-07-22 15:08:19 -0300

    inital-commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## Teste de Desenvolvedor WEB
+
+Para seguirmos com a candidatura, segue em anexo o teste prático. Você terá 48
+horas para realizar o teste a partir do momento de recebimento deste documento.
+O desafio consiste em desenvolver esta tela com base no layout apresentado no
+Figma. Para isso, siga as orientações abaixo:
+ 
+ - Desenvolver essa tela, criando o tema, sem o uso de builders.
+ 
+https://drive.google.com/file/d/1XMHAK1fD784dl82zllRHdynn3jArss8v/view?usp=sha
+ring
---
+++ scripts/tailwind.js
@@ -0,0 +1,16 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'profatto-green': '#131f1d',
+                        'profatto-dark': '#092820',
+                        'profatto-accent': '#8b941f',
+                        'profatto-gray': '#9b9b9b'
+                    }, 
+                    fontFamily: {
+                        'sans': ['Anek Telugu', 'sans-serif'],
+                    }
+                    
+                }
+            }
+        }
